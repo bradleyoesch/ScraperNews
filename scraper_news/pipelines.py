aaefb4e4115974ef6c11926ec9e82da3b3fc66d9
Bradley Oesch <bradley.oesch@gmail.com>
2014-11-18 11:49:21 -0500

    added logging and updates to properly update queue

--- scraper_news/pipelines.py
+++ scraper_news/pipelines.py
@@ -6,20 +6,23 @@
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
 from scrapy.exceptions import DropItem
+import logging
 
 class ScraperNewsPipeline(object):
 
-  min_comments = 50
+  MIN_COMMENTS = 70
 
   def process_item(self, item, spider):
     if (not item['num_comments']):
       raise DropItem("No comments found.")
-    elif (item['num_comments'] < self.min_comments):
-      raise DropItem("Less than %d comments found, dropping item." % self.min_comments)
+    elif (item['num_comments'] < self.MIN_COMMENTS):
+      raise DropItem("Less than {0} comments found, dropping item.".format(self.MIN_COMMENTS))
     elif (not item['item_id']):
       raise DropItem("No ID found.")
     elif (not item['title']):
       raise DropItem("No title found.")
     elif (not item['url']):
       raise DropItem("No url found.")
+    logging.basicConfig(filename='logs.log',level=logging.DEBUG)
+    logging.info("Not dropped: {0}".format(item['item_id']))
     return item
--- scraper_news/spiders/scraper_news_spider.py
+++ scraper_news/spiders/scraper_news_spider.py
@@ -1,4 +1,7 @@
 import scrapy
+import logging
+import time
+import datetime
 
 from scraper_news.items import ScraperNewsItem
 
@@ -8,13 +11,17 @@ class ScraperNewsSpider(scrapy.Spider):
   start_urls = ["https://news.ycombinator.com/"]
 
   def parse(self, response):
-    # index = 0
+    logging.basicConfig(filename='logs.log',level=logging.DEBUG)
+    logging.info("====================")
+    time_now = datetime.datetime.fromtimestamp(time.time()).strftime("%Y-%m-%d %H:%M:%S")
+    logging.info("{0} - Begin crawl".format(time_now))
+
     items = []
 
-    for sel in response.xpath('//body/center/table/tr[position()=3]/td/table/tr[(position() -1) mod 3 = 0 and position() >= 1]'):
+    for sel in response.xpath("//body/center/table/tr[position()=3]/td/table/tr[(position() -1) mod 3 = 0 and position() >= 1]"):
       item = ScraperNewsItem()
 
-      num_comments = sel.xpath('following-sibling::tr[position() = 1]/td[@class="subtext"]/a[last()]/text()').extract()
+      num_comments = sel.xpath("following-sibling::tr[position() = 1]/td[@class=\"subtext\"]/a[last()]/text()").extract()
       if (len(num_comments) > 0):
         num_comments = num_comments[0].split(" ")
         if (len(num_comments) > 1):
@@ -24,19 +31,19 @@ class ScraperNewsSpider(scrapy.Spider):
       else: # no comments
         item['num_comments'] = None
 
-      item_id = sel.xpath('following-sibling::tr[position() = 1]/td[@class="subtext"]/a[last()]').re("\d+")
+      item_id = sel.xpath("following-sibling::tr[position() = 1]/td[@class=\"subtext\"]/a[last()]").re("\d+")
       if (len(item_id) > 0):
         item['item_id'] = int(item_id[0])
       else: # cannot find id
         item['item_id'] = None
 
-      title = sel.xpath('td[@class="title"]/a/text()').extract()
+      title = sel.xpath("td[@class=\"title\"]/a/text()").extract()
       if (len(title) > 0):
-        item['title'] = title[0].replace(u"\u2018", "'").replace(u"\u2019", "'").replace(u"\u201c","\"").replace(u"\u201d", "\"") # remove smart quotes
+        item['title'] = title[0].replace(u"\u2018", "'").replace(u"\u2019", "'").replace(u"\u201c","\"").replace(u"\u201d", "\"").replace(u"\u2013", "-") # remove smart quotes and dashes
       else: # cannot find title
         item['title'] = None
 
-      url = sel.xpath('td[@class="title"]/a/@href').extract()
+      url = sel.xpath("td[@class=\"title\"]/a/@href").extract()
       if (len(url) > 0):
         if (url[:5] == 'item?'): # for self posts
           url = "https://news.ycombinator.com/" + url
@@ -44,10 +51,6 @@ class ScraperNewsSpider(scrapy.Spider):
       else: # cannot find url
         item['url'] = None
 
-      # yield item
       items.append(item)
 
-      # if (index % 1 == 0):
-      #   self.log("\n%d\n%s\n%s" % (index, sel.xpath('following-sibling::tr[position() = 1]/td[@class="subtext"]/a[last()]/text()').extract(), item['num_comments']))
-      # index += 1
     return items
